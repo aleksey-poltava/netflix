36089aeb8ab22a60887262b165dbed39006e8731
Aleksey <poltavaaa@gmail.com>
2022-04-20 18:34:38 +0300

    Header profile
    6.10

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import {Link as ReactRouterLink} from 'react-router-dom';
 
-import {Background, Container, Logo, ButtonLink} from './styles/header';
+import {Background,
+        Container,
+        Logo,
+        ButtonLink,
+        Feature,
+        Text,
+        FeatureCallOut,
+        Link,
+        Picture,
+        Group,
+        Profile,
+        Dropdown
+      } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? (
@@ -13,8 +25,40 @@ export default function Header({ bg = true, children, ...restProps }) {
     );
   }
 
+Header.Feature = function HeaderFeature({children, ...restProps}) {
+  return <Feature {...restProps}>{children}</Feature>;
+}
+
+Header.Profile = function HeaderProfile({children, ...restPros}) {
+  return <Profile {...restPros}>{children}</Profile>;
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({children, ...restProps}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+}
+
+Header.Picture = function HeaderPicture({src, ...restProps}) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+}
+
+Header.Dropdown = function HeaderDropdown({children, ...restProps}) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+}
+
+Header.Text = function HeaderText({children, ...restProps}) {
+  return <Text {...restProps}>{children}</Text>;
+}
+
+Header.TextLink = function HeaderTextLink({children, ...restProps}) {
+  return <Link {...restProps}>{children}</Link>;
+}
+
 Header.Frame = function HeaderFrame({children, ...restProps}) {
     return <Container {...restProps}>{children}</Container>;
+}
+
+Header.Group = function HeaderGroup({children, ...restProps}) {
+  return <Group {...restProps}>{children}</Group>;
 }
 
 Header.ButtonLink = function HeaderButtonLink({children, ...restProps}) {
